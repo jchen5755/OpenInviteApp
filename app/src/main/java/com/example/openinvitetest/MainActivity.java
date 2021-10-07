30823f54abc2e4c761c612730ba6c55ab6387aa1
Jerry Chen <jerrychen5755@gmail.com>
2021-10-06 23:32:32 -0400

    login and sign up screen

--- app/src/main/java/com/example/openinvitetest/MainActivity.java
+++ app/src/main/java/com/example/openinvitetest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.openinvitetest;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Choose_Login_And_Reg.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
     }
 }
