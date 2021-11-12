176aaa75ec68d54fe4c6012268d744da151d890c
Ethan Lee <47604796+eklee2000@users.noreply.github.com>
2021-11-12 17:27:08 -0500

    map permissions?

--- app/src/main/java/com/example/openinvitetest/mainMapFragment.java
+++ app/src/main/java/com/example/openinvitetest/mainMapFragment.java
@@ -1,8 +1,15 @@
 package com.example.openinvitetest;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationRequest;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +22,17 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.location.LocationEngineResult;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 
 //import com.google.android.gms.maps.MapView;
 //import com.mapbox.mapboxsdk.Mapbox;
@@ -31,23 +47,31 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.maps.UiSettings;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.zip.Inflater;
 
-public class mainMapFragment extends Fragment implements View.OnClickListener {
+import timber.log.Timber;
+
+public class mainMapFragment extends Fragment implements LocationEngineCallback<LocationEngineResult>,
+        PermissionsListener, View.OnClickListener {
     private final String TAG = getClass().getSimpleName();
 
-    private Button mCreateInvite, mUserInvite;
-    private ImageButton mProfile;
+    private Button mCreateInvite, mUserInvite, mProfile, mMyLocation;
     private MapView mMapView;
     private MapboxMap mMapboxMap;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
     private FragmentManager fm;
+    private PermissionsManager mPermissionsManager;
+    private LocationEngine mLocationEngine;
 
     @Nullable
     @Override
@@ -63,14 +87,16 @@ public class mainMapFragment extends Fragment implements View.OnClickListener {
                 UiSettings uiSettings = mMapboxMap.getUiSettings();
                 uiSettings.setCompassEnabled(true);
                 uiSettings.setZoomGesturesEnabled(true);
-//                enableLocationComponent();
+                enableLocationComponent();
             });
         });
+
 
         mAuth = FirebaseAuth.getInstance();
         mProfile = v.findViewById(R.id.profileBtn);
         mCreateInvite = v.findViewById(R.id.createInvite);
         mUserInvite = v.findViewById(R.id.userInvite);
+        mMyLocation = v.findViewById(R.id.myLocationBtn);
         fm = getParentFragmentManager();
 
         if (mProfile != null) {
@@ -81,6 +107,8 @@ public class mainMapFragment extends Fragment implements View.OnClickListener {
         }
         if (mUserInvite != null) {
             mUserInvite.setOnClickListener(this);
+        }if (mMyLocation != null) {
+            mMyLocation.setOnClickListener(this);
         }
         firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -89,6 +117,23 @@ public class mainMapFragment extends Fragment implements View.OnClickListener {
         };
         Log.d(TAG, "On Create View portion of Fragment Lifecycle");
         return v;
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    private void enableLocationComponent() {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(getContext())) {
+            LocationComponent locationComponent = mMapboxMap.getLocationComponent();
+            LocationComponentActivationOptions.Builder builder =
+                    new LocationComponentActivationOptions.Builder(getContext(),
+                            Objects.requireNonNull(mMapboxMap.getStyle()));
+            builder.useDefaultLocationEngine(true);
+            LocationComponentActivationOptions options = builder.build();
+            locationComponent.activateLocationComponent(options);
+        } else {
+            mPermissionsManager = new PermissionsManager(this);
+            mPermissionsManager.requestLocationPermissions(getActivity());
+        }
     }
 
 
@@ -100,6 +145,8 @@ public class mainMapFragment extends Fragment implements View.OnClickListener {
                     .replace(R.id.fragment_container, new ProfileFragment())
                     .addToBackStack("profile_fragment")
                     .commit();
+        } else if (viewId == R.id.myLocationBtn) {
+            requestLocation();
         }
     }
 
@@ -113,5 +160,86 @@ public class mainMapFragment extends Fragment implements View.OnClickListener {
     public void onStop() {
         super.onStop();
         mAuth.removeAuthStateListener(firebaseAuthStateListener);
+    }
+
+    private void requestLocation() {
+        Timber.d("requestLocation()");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!PermissionsManager.areLocationPermissionsGranted(getContext())) {
+                int PERMISSION_REQUEST_LOCATION = 1;
+                mPermissionsManager = new PermissionsManager(this);
+                mPermissionsManager.requestLocationPermissions(getActivity());
+            } else {
+                doRequestLocation();
+            }
+        } else {
+            doRequestLocation();
+        }
+    }
+
+    private void doRequestLocation() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mLocationEngine = initializeLocationEngine();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private LocationEngine initializeLocationEngine() {
+        LocationEngine locationEngine = LocationEngineProvider.getBestLocationEngine(getContext());
+        LocationEngineRequest.Builder locRequestBuilder = new LocationEngineRequest.Builder(60000);
+//        locRequestBuilder.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        LocationEngineRequest locRequest = locRequestBuilder.build();
+
+        if (hasLocationPermission()) {
+            locationEngine.requestLocationUpdates(locRequest, this, Looper.getMainLooper());
+        }
+
+        return locationEngine;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean lacksLocationPermission() {
+        return !PermissionsManager.areLocationPermissionsGranted(getContext());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean hasLocationPermission() {
+        return PermissionsManager.areLocationPermissionsGranted(getContext());
+    }
+
+//    @RequiresApi(api = Build.VERSION_CODES.M)
+//    private boolean lacksLocationPermission() {
+//        return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED;
+//    }
+//
+//    @RequiresApi(api = Build.VERSION_CODES.M)
+//    private boolean hasLocationPermission() {
+//        return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED;
+//    }
+
+    //TODO
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+
+    }
+
+    @Override
+    public void onSuccess(LocationEngineResult result) {
+        Location location = result.getLastLocation();
+        mMapboxMap.
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception exception) {
+
     }
 }
