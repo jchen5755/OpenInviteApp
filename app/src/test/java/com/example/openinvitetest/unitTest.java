799af6629d6bdcb03cd9332feee741470cb29e91
Ethan Lee <47604796+eklee2000@users.noreply.github.com>
2021-11-29 15:29:07 -0500

    working unit test

--- app/src/test/java/com/example/openinvitetest/unitTest.java
+++ app/src/test/java/com/example/openinvitetest/unitTest.java
@@ -1,2 +1,21 @@
-package com.example.openinvitetest;public class unitTest {
+package com.example.openinvitetest;
+import android.content.Context;
+import org.junit.Test;
+
+import com.google.common.truth.Truth;
+
+public class unitTest {
+    private static final String userID = "abc";
+
+    @Test
+    public void readStringFromContext_LocalizedString() {
+        // Given a Context object retrieved from Robolectric...
+        userIdViewModel myObjectUnderTest = new userIdViewModel();
+
+        // ...when the string is returned from the object under test...
+        myObjectUnderTest.setUserID(userID);
+
+        // ...then the result should be the expected one.
+        Truth.assertThat("abc").isEqualTo(myObjectUnderTest.returnID());
+    }
 }
